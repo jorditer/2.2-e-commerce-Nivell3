712c12fddf4c04a7885fb60553e052d0a2cbf588
jordit <jordi.terradas6@gmail.com>
2024-09-30 00:18:52 +0200

    level2

--- js/checkout.js
+++ js/checkout.js
@@ -5,23 +5,62 @@ function validate() {
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
 	var errorEmail = document.getElementById("errorEmail");  
+	var errorAddress = document.getElementById("errorAddress");
+	var errorLastN = document.getElementById("errorLastN");
+	var errorPassword = document.getElementById("errorPassword");
+	var errorPhone = document.getElementById("errorPhone");
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	errorName.classList.remove('d-block');
+	errorEmail.classList.remove('d-block');
+	errorAddress.classList.remove('d-block');
+	errorLastN.classList.remove('d-block');
+	errorPassword.classList.remove('d-block');
+	errorPhone.classList.remove('d-block');
+
+	if(fName.value == "" || fName.value.length < 3){
+		errorName.classList.add('d-block');
+		error++;
+	} 
+
+	if(fEmail.value == "" || !/^(?=.*@).{3,}$/.test(fEmail.value)){
+		errorEmail.classList.add('d-block');
 		error++;
 	}
 
-	if(fEmail.value == ""){
+	if(fAddress.value == "" || fAddress.value.length < 3){
+		errorAddress.classList.add('d-block');
 		error++;
 	}
-	 
+
+	if(fLastN.value == "" || fLastN.value.length < 3){
+		errorLastN.classList.add('d-block');
+		error++;
+	}
+
+
+	if(fPassword.value == "" || !/(?=.*\d)(?=.*[a-zA-Z]).{3,}/.test(fPassword.value)){
+		errorPassword.classList.add('d-block');
+		error++;
+	}
+
+	if(fPhone.value == "" || !/^\d{9}$/.test(fPhone.value.trim())){
+		errorPhone.classList.add('d-block');
+		error++;
+	}
+
 	if(error>0){
+		event.preventDefault();
 		alert("Error");
-	}else{
+	} else {
 		alert("OK");
 	}
 
--- js/shop.js
+++ js/shop.js
@@ -76,38 +76,99 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+    for (let product of products) {
+        if (product.id === id) {
+            let carProduct = cart.find(obj => obj.id == id);
+            if (carProduct){
+                carProduct.quantity++;
+            } else {
+                cart.push({...product, quantity : 1});
+            }
+            break;
+        }
+    }
 }
+// 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cart array
 
 // Exercise 2
 function cleanCart() {
-
+    cart.length = 0;
+	total = 0;
+    printCart();
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+	total = 0;
+    for (let product of cart){
+        total += (product.price * product.quantity);
+    }
+	total = total.toFixed(2);
 }
 
 // Exercise 4
+// Si l'usuari/ària compra 3 o més ampolles d'oli, el preu del producte es rebaixa un 20%.
+// Quan es compren 10 o més productes per a fer pastissos, el preu del producte es rebaixa un 30%.
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+	for (let product of cart){
+		if (product.id === 1){  //cooking oil
+            if (product.quantity >= 3) {
+                product.price = products.find(product => product.id == 1).price * 0.8;
+            } else {
+                product.price = products.find(product => product.id == 1).price;  // Si quito del carrito y ahora es < 3 quitar descuento
+            }
+		} else if (product.id === 3){ //muffins
+            if (product.quantity >= 10){
+                product.price = products.find(product => product.id == 3).price * 0.7;
+            } else {
+                product.price = products.find(product => product.id == 3).price; // Si quito del carrito y ahora es < 10 quitar descuento
+            }
+		}
+	}
 }
 
 // Exercise 5
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    const totalPrice = document.querySelector('#total_price');
+    totalPrice.innerText = `${total}`;
+    const tbody = document.querySelector('#cart_list');
+    tbody.innerHTML = '';
+    let cartHTML = '';
+    for (const item of cart){
+        cartHTML += 
+            `<tr>
+                <th scope=row>${item.name}</th>
+                <td>${item.price.toFixed(2)}</td>
+                <td>${item.quantity}</td>
+                <td>${(item.quantity * item.price).toFixed(2)}</td>
+                <td><button class="btn btn-primary" onclick=removeFromCart(${item.id})>Remove</button><td> 
+            </tr>`;
+    }
+    tbody.insertAdjacentHTML('beforeend', cartHTML)
 }
-
 
 // ** Nivell II **
 
 // Exercise 7
 function removeFromCart(id) {
-
+    for (const [index, item] of cart.entries()){
+        if (item.id == id) {
+            if (item.quantity > 1){
+                item.quantity--;
+            } else {
+                cart.splice(index, 1);
+            }
+            break;
+        }
+    }
+    open_modal();
 }
 
 function open_modal() {
+	calculateTotal();
+	applyPromotionsCart();
     printCart();
 }
